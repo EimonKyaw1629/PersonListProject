805026da0db8b94930d80aad4aa735f3cbd84287
EimonKyaw1629 <43800422+EimonKyaw1629@users.noreply.github.com>
2018-10-19 14:09:13 +0900

    image remove in edit page

--- PersonListProject/src/main/java/com/example/personlist/mapper/UploadFileMapper.java
+++ PersonListProject/src/main/java/com/example/personlist/mapper/UploadFileMapper.java
@@ -13,7 +13,7 @@ public class UploadFileMapper implements RowMapper<MyUploadForm>{
 	public static final String file_UPDATE_SQL = "Update Tb_File set uploadRootPath=?,name=?,serverFile=? where PersonID=?";
 	public static final String file_Delete_Sql = "Delete from Tb_File where PersonID=?";
 	public static final String file_XMLSelect_Sql = "SELECT  FileID,PersonID,uploadRootPath,name,serverFile  FROM Tb_Address FOR XML AUTO";
-	public static final String file_INSERT_SQL = "insert into Tb_File(personID,uploadRootPath,name,serverFile) values(?,?,?,?,?)";
+	public static final String file_INSERT_SQL = "insert into Tb_File(personID,uploadRootPath,name,serverFile) values(?,?,?,?)";
 	public static final String file_Select_Sql = "SELECT  FileID,PersonID,uploadRootPath,name,serverFile  FROM Tb_File ";
 	@Override
 	public MyUploadForm mapRow(ResultSet rs, int rowNum) throws SQLException {
