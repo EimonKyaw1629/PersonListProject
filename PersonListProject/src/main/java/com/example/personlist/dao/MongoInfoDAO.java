7ffa892777263bb913404bcaf287e8f0d24cb9a6
EimonKyaw1629 <43800422+EimonKyaw1629@users.noreply.github.com>
2018-11-05 15:25:18 +0900

    add Convert bson Document to object model

--- PersonListProject/src/main/java/com/example/personlist/dao/MongoInfoDAO.java
+++ PersonListProject/src/main/java/com/example/personlist/dao/MongoInfoDAO.java
@@ -2,8 +2,11 @@ package com.example.personlist.dao;
 
 import static com.mongodb.client.model.Filters.eq;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -42,7 +45,7 @@ public class MongoInfoDAO extends JdbcDaoSupport {
 		return collection;
 	}
 	
-	private Document mongoDoc(MongoInfo mongoinfo) {
+	private Document ConvertObjToDoc(MongoInfo mongoinfo) {
 		ObjectMapper oMapper = new ObjectMapper();
 	    Map<String, Object> map = oMapper.convertValue(mongoinfo, new TypeReference<Map<String, Object>>() {});
 	    Document company = new Document(new HashMap<String, Object>());
@@ -50,16 +53,54 @@ public class MongoInfoDAO extends JdbcDaoSupport {
 		return company;
 	}
 	
+	private MongoInfo ConvertDocToMongo(Document mongoinfo) {
+		//Map<String, Object> map = new HashMap<>(mongoinfo);
+		Map<String, Object> map = new HashMap<>(mongoinfo);
+		MongoInfo info = new MongoInfo();
+		info= (MongoInfo) MongoInfoDAO.convertMapToObject(map, info);
+		return info;
+	}
+	public static Object convertMapToObject(Map<String, Object> map, Object objClass){
+		String keyAttribute = null;
+		String setMethodString = "set";
+		String methodString = null;
+		Iterator<String> itr = map.keySet().iterator();
+		while(itr.hasNext()){
+			keyAttribute = (String) itr.next();
+			methodString = setMethodString+keyAttribute.substring(0,1).toUpperCase()+keyAttribute.substring(1);
+			try {
+				Method[] methods = objClass.getClass().getDeclaredMethods();
+				for(int i=0;i<=methods.length-1;i++){
+					if(methodString.equals(methods[i].getName())){
+						System.out.println("invoke : "+methodString);
+						methods[i].invoke(objClass, map.get(keyAttribute));
+					}
+				}
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}
+		return objClass;
+	}
+
+
+	
 	
 	public void mongoInsert(MongoInfo mongoinfo) {
         MongoCollection<Document> collection = this.getMongoDatabase();
-        Document mongodoc = this.mongoDoc(mongoinfo);
+        Document mongodoc = this.ConvertObjToDoc(mongoinfo);
         collection.insertOne(mongodoc);
 	}
 	
 	public void mongoUpdata(MongoInfo mongoinfo) {
 		MongoCollection<Document> collection = this.getMongoDatabase();
-		Document mongodoc = this.mongoDoc(mongoinfo);
+		Document mongodoc = this.ConvertObjToDoc(mongoinfo);
         BasicDBObject whereQuery = new BasicDBObject();
         whereQuery.put("id", mongoinfo.getId());
         collection.findOneAndReplace(whereQuery, mongodoc);
@@ -79,7 +120,7 @@ public class MongoInfoDAO extends JdbcDaoSupport {
 			List<MongoInfo> gList = new ArrayList<MongoInfo>();
 			
 			for (Document gender : listGender) {
-				MongoInfo mongoinfo = new MongoInfo((int)gender.getInteger("id"),gender.getString("gender"),gender.getInteger("age"),gender.getString("job"));
+				MongoInfo mongoinfo = this.ConvertDocToMongo(gender); //new MongoInfo((int)gender.getInteger("id"),gender.getString("gender"),gender.getInteger("age"),gender.getString("job"));
 				
 				gList.add(mongoinfo);
 			}
@@ -93,7 +134,7 @@ public class MongoInfoDAO extends JdbcDaoSupport {
         try {
         	MongoCollection<Document> collection = this.getMongoDatabase();
             Document res = collection.find(eq("id", pid)).first();
-            MongoInfo info = new MongoInfo(res.getInteger("id"),res.getString("gender"),res.getInteger("age"),res.getString("job"));   
+            MongoInfo info = this.ConvertDocToMongo(res);   
             return info;
         } catch (Exception ex) {
             return null;
@@ -112,7 +153,7 @@ public class MongoInfoDAO extends JdbcDaoSupport {
 			List<Document> findList = (List<Document>) collection.find(whereQuery).into(new ArrayList<Document>());
 			List<MongoInfo> gList = new ArrayList<MongoInfo>();
 			for (Document lst : findList) {
-				MongoInfo mongoinfo = new MongoInfo(lst.getInteger("id"),lst.getString("gender"),lst.getInteger("age"),lst.getString("job"));
+				MongoInfo mongoinfo = this.ConvertDocToMongo(lst);
 				gList.add(mongoinfo);
 			}
 			return gList;
